6d0f8d654b343944f8541b0695de639e61252cfd
shepardxia <78109174+shepardxia@users.noreply.github.com>
2026-09-15 17:34:02 -0400

    Point the formula at the v1.0.4 release

--- Formula/ruecksicht.rb
+++ Formula/ruecksicht.rb
@@ -1,8 +1,8 @@
 class Ruecksicht < Formula
   desc "Desktop widget host that renders JavaScript widgets behind your windows"
   homepage "https://github.com/shepardxia/ruecksicht"
-  url "https://github.com/shepardxia/ruecksicht/releases/download/v1.0.3/ruecksicht-1.0.3.tar.gz"
-  sha256 "9bedb720034c7932eb37cbf30c45df31e656bb8cc78275851d0d7f764d53645b"
+  url "https://github.com/shepardxia/ruecksicht/releases/download/v1.0.4/ruecksicht-1.0.4.tar.gz"
+  sha256 "541bebdfeeebbd1eccf20c9fb07af1eb892f85c3d7257423ae93dededd564b28"
   license "GPL-3.0-or-later"
   head "https://github.com/shepardxia/ruecksicht.git", branch: "master"
 
